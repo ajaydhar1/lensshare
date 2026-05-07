faab84f4307a1d0182eccf5b314e19cde190e353
Ajay Dhar <ajaydhar108@gmail.com>
2026-05-07 00:19:45 -0400

    Update GA4 analytics tracking ID for LensShare

---
+++ ___config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Domain
+$site_name = "LensShare";
+$site_domain = "lensshare.co";
+// Replace with your own background image. It should go in the folder assets/img/home/
+// and be used instead of assets/img/home/lensshare-print.png
+$site_name_font_on_homepage = "lensshare-print.png";
+
+// API keys
+$giphy_api_key = "GCVjGzB6d2XHhoXMC8NnxrPvnDrxImcX";
+$twitch_client_id = "04p46rqs0xn1rlmqpfvhbt2buvlk4r";
+$twitch_client_secret = "zmkimtl7ut9b8afl4p6j499hmfwoby";
+
+// Google Analytics
+$ga_measurement_id = "G-6XLL8VCVZS";
+
+?>
